10f6dd0d2b4482982247579387dd0ef4908d1cff
Anıl Aydın <anil_aydinn@outlook.com>
2022-05-05 14:01:47 +0300

    [Anıl] Remove unrequired html

--- src/components/Profile/ProfileContent.js
+++ src/components/Profile/ProfileContent.js
@@ -53,9 +53,9 @@ const ProfileContent = (props) => {
                       src="https://bootdey.com/img/Content/avatar/avatar6.png"
                       alt="profile"
                     />
-                    <span className="profile-name">{`${user && user.name} ${
-                      user && user.surname
-                    }`}</span>
+                    <span className="profile-name text-white">{`${
+                      user && user.name
+                    } ${user && user.surname}`}</span>
                   </div>
                   <div className="d-none d-md-block">
                     <Link to={"/profile/edit"} className={classes.link}>
@@ -118,105 +118,11 @@ const ProfileContent = (props) => {
           <div className="d-none d-md-block col-md-4 col-xl-3 left-wrapper">
             <div className="card rounded">
               <div className="card-body">
-                <div className="d-flex align-items-center justify-content-between mb-2">
-                  <h6 className="card-title mb-0">About</h6>
-                  <div className="dropdown">
-                    <button
-                      className="btn p-0"
-                      type="button"
-                      id="dropdownMenuButton"
-                      data-toggle="dropdown"
-                      aria-haspopup="true"
-                      aria-expanded="false"
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        className="feather feather-more-horizontal icon-lg text-muted pb-3px"
-                      >
-                        <circle cx="12" cy="12" r="1"></circle>
-                        <circle cx="19" cy="12" r="1"></circle>
-                        <circle cx="5" cy="12" r="1"></circle>
-                      </svg>
-                    </button>
-                    <div
-                      className="dropdown-menu"
-                      aria-labelledby="dropdownMenuButton"
-                    >
-                      <a
-                        className="dropdown-item d-flex align-items-center"
-                        href="#"
-                      >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="24"
-                          height="24"
-                          viewBox="0 0 24 24"
-                          fill="none"
-                          stroke="currentColor"
-                          stroke-width="2"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          className="feather feather-edit-2 icon-sm mr-2"
-                        >
-                          <path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path>
-                        </svg>
-                        <span className="">Edit</span>
-                      </a>
-                      <a
-                        className="dropdown-item d-flex align-items-center"
-                        href="#"
-                      >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="24"
-                          height="24"
-                          viewBox="0 0 24 24"
-                          fill="none"
-                          stroke="currentColor"
-                          stroke-width="2"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          className="feather feather-git-branch icon-sm mr-2"
-                        >
-                          <line x1="6" y1="3" x2="6" y2="15"></line>
-                          <circle cx="18" cy="6" r="3"></circle>
-                          <circle cx="6" cy="18" r="3"></circle>
-                          <path d="M18 9a9 9 0 0 1-9 9"></path>
-                        </svg>
-                        <span className="">Update</span>
-                      </a>
-                      <a
-                        className="dropdown-item d-flex align-items-center"
-                        href="#"
-                      >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="24"
-                          height="24"
-                          viewBox="0 0 24 24"
-                          fill="none"
-                          stroke="currentColor"
-                          stroke-width="2"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          className="feather feather-eye icon-sm mr-2"
-                        >
-                          <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-                          <circle cx="12" cy="12" r="3"></circle>
-                        </svg>
-                        <span className="">View all</span>
-                      </a>
-                    </div>
+                {user && user.description && (
+                  <div className="d-flex align-items-center justify-content-between mb-2">
+                    <h6 className="card-title mb-0">About</h6>
                   </div>
-                </div>
+                )}
                 {user && user.description && (
                   <p>
                     Hi! I'm Amiah the Senior UI Designer at Vibrant. We hope you
