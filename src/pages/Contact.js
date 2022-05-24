92a3479537e98169e1e6664a7961a0d9d5fe0fd5
Anıl Aydın <anil_aydinn@outlook.com>
2022-05-25 02:31:17 +0300

    [Anıl] Add search user results component to contact page.

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,14 +1,25 @@
 import React from "react";
 import ContactBox from "../components/Contact/ContactBox";
 import Header from "../components/General/Header";
+import { connect } from "react-redux";
+import SearchedUsers from "../components/SearchedUsers/SearchedUsers";
 
-const Contact = () => {
+const Contact = (props) => {
+  const { searchedUsers } = props;
+
   return (
     <div>
       <Header />
-      <ContactBox />
+      {searchedUsers && searchedUsers.length > 0 && <SearchedUsers />}
+      {!searchedUsers && <ContactBox />}
     </div>
   );
 };
 
-export default Contact;
+const mapStateToProps = (state) => ({
+  searchedUsers: state.user.searchedUsers,
+});
+
+const mapDispatchToProps = (dispatch) => {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Contact);
