b22cdf67f5c995ec0876114edae9ceda87971bf7
Anıl Aydın <anil_aydinn@outlook.com>
2022-06-04 20:32:23 +0300

    [Anıl] Remove unused import.

--- src/components/Admin/ContactsList.js
+++ src/components/Admin/ContactsList.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
-import { Link } from "react-router-dom";
 import { deleteContact } from "../../api/contactApi";
 import { fetchContacts } from "../../redux/actions/contactActions";
 
